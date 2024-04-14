4711bacfc1914d2c227106355490d0947ef42fac
Lê Quốc Hưng <98202931+hungle2002@users.noreply.github.com>
2024-04-14 22:44:23 +0700

    [Harry] Implement text view

--- app/src/main/java/com/example/demoiotdashboard/MainActivity.java
+++ app/src/main/java/com/example/demoiotdashboard/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
 import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
@@ -12,11 +13,16 @@ import org.eclipse.paho.client.mqttv3.MqttMessage;
 public class MainActivity extends AppCompatActivity {
 
     MQTTHelper mqttHelper;
+    TextView txtTemp, txtHumi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtTemp = findViewById(R.id.txtTemperature);
+        txtHumi = findViewById(R.id.txtHumidity);
+
         startMQTT();
     }
 
@@ -36,6 +42,11 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void messageArrived(String topic, MqttMessage message) throws Exception {
                 Log.d("TEST", topic + "***" + message.toString());
+//                if(topic.contains("cambien1")){
+//                    txtTemp.setText(message.toString() + "°C");
+//                } else if (topic.contains("cambien2")){
+//                    txtHumi.setText(message.toString() + "%");
+//                }
             }
 
             @Override
